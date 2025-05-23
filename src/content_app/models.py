7df985f115596b6cc9fe7614beef7e231a1d5632
Lucas Alves <lucasacastela@gmail.com>
2025-05-23 20:48:05 -0300

    Pedido, Pagamento, Motorista

--- src/content_app/models.py
+++ src/content_app/models.py
@@ -34,14 +34,39 @@ class Produto(models.Model):
     def __str__(self):
         return self.nome
 
+class Motorista(models.Model):
+    nome = models.CharField(max_length=100)
+    cpf_motorista = models.CharField(max_length=11, unique= True)
+    data_nascimento_motorista = models.DateField()
+    veiculo = models.CharField(max_length=30)
+    telefone = models.CharField(max_length=9)
+    email = models.CharField(max_length=30)
+    cnh = models.CharField(max_length=11, unique= True)
+    numero_registro = models.CharField(max_length=11, unique= True)
+    senha_motorista = models.CharField(max_length=30)
+
+
+    def __str__(self):
+        return self.nome
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11, unique=True)
+    data_nascimento = models.DateField()
+    premium = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
+
 class Pedido(models.Model):
     descricao = models.TextField(blank=True, null=True)  # Descrição do pedido
-    cliente = models.ForeignKey(User, on_delete=models.CASCADE, related_name='pedido')  # Quem fez o pedido
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, related_name='pedidos')  # Quem fez o pedido
     produtos = models.ManyToManyField(Content, related_name='pedido')  # Produtos incluídos no pedido
     preco = models.DecimalField(max_digits=6, decimal_places=2) #somatório do preço dos pedidos
     data_de_criacao = models.DateTimeField(auto_now_add=True)  # Data de criação
     ultima_atualizacao = models.DateTimeField(auto_now=True)  # Data de última atualização
     status = models.CharField(max_length=20, default='Em processo') # O status do pedido
+    motorista = models.ForeignKey(Motorista, related_name='pedidos_entregues', on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['data_de_criacao']
@@ -52,14 +77,7 @@ class Pedido(models.Model):
         return self.cliente
 
 
-class Cliente(models.Model):
-    nome = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=11, unique=True)
-    data_nascimento = models.DateField()
-    premium = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.nome
 
 class Loja(models.Model):
     nome = models.CharField(max_length=100)
@@ -75,29 +93,15 @@ class Loja(models.Model):
     def __str__(self):
         return self.nome
 
-
-class Motorista(models.Model):
-    nome = models.CharField(max_length=100)
-    cpf_motorista = models.CharField(max_length=11, unique= True)
-    data_nascimento_motorista = models.DateField()
-    veiculo = models.CharField(max_length=30)
-    telefone = models.CharField(max_length=9)
-    email = models.CharField(max_length=30)
-    cnh = models.CharField(max_length=11, unique= True)
-    numero_registro = models.CharField(max_length=11, unique= True)
-    senha_motorista = models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.nome
-
 class Pagamento(models.Model):
     valor = models.DecimalField(max_digits=6, decimal_places=2)
     data_pagamento = models.DateTimeField(default=timezone.now)
     metodo_pagamento = models.CharField(max_length=50)
     status = models.CharField(max_length=20, default='Pendente')
+    pedido = models.OneToOneField(Pedido, on_delete=models.CASCADE, related_name='pagamento')
 
     def  __str__(self):
-            return self.valor
+            return f'Pagamento #{self.id} - R$ {self.valor}'
 
 class Avaliacao(models.Model):
     nome_cliente = models.CharField(max_length=100)
